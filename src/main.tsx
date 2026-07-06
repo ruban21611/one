7b449224bd99e8141215461cdebaf8b2e43f15df
ruban21611 <ruban21611@gmail.com>
2026-07-06 15:12:55 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/service-worker.js')
-      .then(() => console.log('✅ PWA Ready'))
-      .catch((err) => console.log('❌ Service Worker Error:', err));
+      .then(() => console.log('PWA Ready'))
+      .catch((err) => console.log('err'));
   });
 }
