a69749cb6f67610f858fb2328dce7d2031b83446
ruban21611 <ruban21611@gmail.com>
2026-07-08 19:09:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ExploreJobs from './components/ExploreJobs';
 import JobInformation from './components/JobInformation';
 import Requesting from './components/Requesting';
 import Notifications from './components/Notifications';
-import FlowVisual from 'components/FlowVisual';
+import FlowVisual from './components/FlowVisualizer';
 import Profile from './components/Profile';
 import { Briefcase, LayoutDashboard, Search, FileText, Bell, User, Info } from 'lucide-react';
 
